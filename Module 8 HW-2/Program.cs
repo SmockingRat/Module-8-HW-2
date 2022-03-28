91f9fae572282243e6ff0edf12b68c96aa29623e
SmokingRat <halfbrikbarry@gmail.com>
2022-03-28 22:55:19 +0300

    Final

--- Module 8 HW-2/Program.cs
+++ Module 8 HW-2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Module_8_HW_2
 {
@@ -6,8 +7,59 @@ namespace Module_8_HW_2
     {
         static void Main(string[] args)
         {
-            
+
+            Console.Write("Enter path to directory ");
+            long Size = 0;
+            bool Existence = false;
+            string PathToDir = Console.ReadLine();
+            Program.ShowDirVolume(PathToDir, ref Size, ref Existence);
+
+            if (Existence)
+            {
+                Console.WriteLine($"Size of directory with included files: {Size} bytes");
+            }
+
             Console.ReadKey();
         }
+
+
+        public static void ShowDirVolume(string path, ref long size, ref bool Existence)
+        {
+            DirectoryInfo dr = new DirectoryInfo(path);
+            try
+            {
+                if (dr.Exists)
+                {
+
+                    FileInfo[] files = dr.GetFiles();
+                    foreach (FileInfo file in files)
+                    {
+                        FileInfo f = new FileInfo(file.FullName);
+                        size += f.Length;
+                    }
+
+                    DirectoryInfo[] direc = dr.GetDirectories();
+                    foreach (DirectoryInfo dir in direc)
+                    {
+
+                        Program.ShowDirVolume(dir.FullName, ref size, ref Existence);
+                    }
+
+                    Existence = true;
+                }
+                else
+                {
+                    Console.WriteLine("User, this directory does not exist");
+                    Existence =  false;
+                }
+            }
+            catch(Exception message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+
+
     }
 }
